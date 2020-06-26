c3e4db566b11f9ef5a330ab2bf644b103e1eed76
DavidFaraday <iosdevschool@gmail.com>
2020-06-26 22:46:05 +0300

    Initial Commit

---
+++ LetsMeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsMeet
+//
+//  Created by David Kababyan on 26/06/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
